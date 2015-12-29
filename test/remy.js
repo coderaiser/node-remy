7492c29796cecab59fb1b8947630d3db4419e5e5
coderaiser <mnemonic.enemy@gmail.com>
2015-12-29 12:50:05 -0500

    test(remy) folder: error SOME_ERROR

--- test/remy.js
+++ test/remy.js
@@ -2,7 +2,11 @@
     'use strict';
     
     var test        = require('tape'),
-        remimitter  = require('..');
+        remimitter  = require('..'),
+         
+        os          = require('os'),
+        path        = require('path'), 
+        fs          = require('fs');
     
     test('file: error EACESS', function(t) {
         var rm = remimitter('/bin/ls');
@@ -26,6 +30,35 @@
         });
         
         rm.on('end', function() {
+            t.end();
+        });
+    });
+    
+    test('folder: error SOME_ERROR', function(t) {
+        var rm,
+            code    = 'SOME_ERROR',
+            rmdir   = fs.rmdir,
+            name    = path.join(os.tmpdir(), String(Math.random()));
+         
+        fs.mkdirSync(name);
+        
+        fs.rmdir = function(name, callback) {
+            var error   = Error('Some error');
+            error.code  = code;
+            
+            callback(error);
+        };
+        
+        rm = remimitter(name);
+        
+        rm.on('error', function(error) {
+            t.equal(error.code, code, error.message);
+            rm.abort();
+        });
+        
+        rm.on('end', function() {
+            fs.rmdir = rmdir;
+            fs.rmdirSync(name);
             t.end();
         });
     });
